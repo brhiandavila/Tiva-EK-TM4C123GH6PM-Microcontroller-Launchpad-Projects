388c4def5d0f21faf7a3bfd1150db1ec6c86ed25
Brhian Davila <chuchilili2003@gmail.com>
2026-03-06 21:05:45 -0500

    Add LED Blink Controller - First FreeRTOS project with 3 tasks, queue communication, and button interrupts

---
+++ Debug/croutine.d
@@ -0,0 +1,72 @@
+# FIXED
+
+croutine.obj: C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/croutine.c
+croutine.obj: C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/FreeRTOS.h
+croutine.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stddef.h
+croutine.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+croutine.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+croutine.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+croutine.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+croutine.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+croutine.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+croutine.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+croutine.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+croutine.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+croutine.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+croutine.obj: C:/Users/chuch/workspace_v12/led_blink_controller/FreeRTOSConfig.h
+croutine.obj: C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/projdefs.h
+croutine.obj: C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/portable.h
+croutine.obj: C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/deprecated_definitions.h
+croutine.obj: C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/portable/CCS/ARM_CM4F/portmacro.h
+croutine.obj: C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/mpu_wrappers.h
+croutine.obj: C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/task.h
+croutine.obj: C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/list.h
+croutine.obj: C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/croutine.h
+croutine.obj: C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/list.h
+
+C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/croutine.c:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/FreeRTOS.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stddef.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+C:/Users/chuch/workspace_v12/led_blink_controller/FreeRTOSConfig.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/projdefs.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/portable.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/deprecated_definitions.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/portable/CCS/ARM_CM4F/portmacro.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/mpu_wrappers.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/task.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/list.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/croutine.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/list.h:
+
